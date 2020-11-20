6b5f6175031186c4595018d31d0737d20e257d15
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-11-20 15:08:15 +0200

    добавляет базовое решение ДЗ

--- js/jsRealization.js
+++ js/jsRealization.js
@@ -1,18 +1,47 @@
 import galleryImg from './gallery-items.js';
 
 const refs = {
-    jsGallery: document.querySelector('.js-gallery'),
+    jsGallery: document.querySelector('ul.js-gallery'),
+    jsLightbox: document.querySelector('.js-lightbox'),
+    jsLightboxImage: document.querySelector('.lightbox__image'),
+    closeLightbox: document.querySelector('button[data-action="close-lightbox"]'),
 }
 
-const addImg = refs.jsGallery.insertAdjacentHTML('beforeend', galleryImg.map(({ preview }, { description }) => 
+const addImg = refs.jsGallery.insertAdjacentHTML('beforeend', galleryImg.map(({ preview, original, description }) =>
     `<li class="gallery__item">
-     <a class="gallery__link" href="#">
+     <a class="gallery__link" href="${original}">
      <img class="gallery__image"
       src="${preview}"
-      data-source="#"
+      data-source="${original}"
       alt="${description}">
     </a>
-    </li>`).join(''))
+    </li>`)
+    .join(''));
 
-console.log(refs.jsGallery)
+function addModalSrc(event) {
+    refs.jsLightboxImage.src = event.target.dataset.source; 
+}; 
 
+function cleanModalSrc() {
+    refs.jsLightboxImage.src = ''; 
+};
+
+function modalIsOpen(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName === 'IMG') {
+        refs.jsLightbox.classList.add('is-open')
+    };
+
+    addModalSrc(event);
+} 
+
+function modalIsClose() {
+    refs.jsLightbox.classList.remove('is-open');
+
+    cleanModalSrc();
+} 
+
+refs.jsGallery.addEventListener('click', modalIsOpen);
+
+refs.closeLightbox.addEventListener('click', modalIsClose);
