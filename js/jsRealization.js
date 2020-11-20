5274959ddb788fbd0e8716039121c84185f8cbff
Tkachenko Anton <tkachenko.anton.90@gmail.com>
2020-11-20 19:30:50 +0200

    частично решает задачу переключения картинок

--- js/jsRealization.js
+++ js/jsRealization.js
@@ -8,13 +8,14 @@ const refs = {
     closeLightbox: document.querySelector('button[data-action="close-lightbox"]'),
 }
 
-const addImg = refs.jsGallery.insertAdjacentHTML('beforeend', galleryImg.map(({ preview, original, description }) =>
+refs.jsGallery.insertAdjacentHTML('beforeend', galleryImg.map(({ preview, original, description }, i) =>
     `<li class="gallery__item">
      <a class="gallery__link" href="${original}">
      <img class="gallery__image"
       src="${preview}"
       data-source="${original}"
-      alt="${description}">
+      alt="${description}"
+      data-index="${i}">
     </a>
     </li>`)
     .join(''));
@@ -22,15 +23,18 @@ const addImg = refs.jsGallery.insertAdjacentHTML('beforeend', galleryImg.map(({ 
 function addModalSrc(event) {
     refs.jsLightboxImage.src = event.target.dataset.source;
     refs.jsLightboxImage.alt = event.target.alt;
+    refs.jsLightboxImage.setAttribute('data-index', `${event.target.dataset.index}`);
 }; 
 
 function cleanModalSrc() {
     refs.jsLightboxImage.src = '#';
     refs.jsLightboxImage.alt = '';
+    refs.jsLightboxImage.removeAttribute('data-index');
 };
 
 function modalIsOpen(event) {
     event.preventDefault();
+
     if (event.target.nodeName !== 'IMG') {
         return;   
     };
@@ -38,6 +42,7 @@ function modalIsOpen(event) {
     refs.jsLightbox.classList.add('is-open');
 
     addModalSrc(event);
+
 } 
 
 function modalIsClose() {
@@ -55,6 +60,25 @@ function keyModalIsClose(event) {
     
 }; 
 
+function keyPressNext(event) {
+    let jsGalleryImg = document.querySelectorAll('.gallery__image');
+    let currentIndex = refs.jsLightboxImage.dataset.index;
+    let nextImg = Number(currentIndex);
+
+    if (refs.jsLightbox.classList.contains('is-open') && event.code === 'ArrowRight') {
+        refs.jsLightboxImage.src = `${(jsGalleryImg[`${nextImg += 1}`]).dataset.source}`;
+    }
+    
+};
+
+// function keyPressPrev(event) {
+
+//     if (refs.jsLightbox.classList.contains('is-open') && event.code === 'ArrowLeft') {
+//         console.log('left')
+//     }
+    
+// };
+
 refs.jsGallery.addEventListener('click', modalIsOpen);
 
 refs.lightboxOverlay.addEventListener('click', modalIsClose);
@@ -63,5 +87,7 @@ refs.closeLightbox.addEventListener('click', modalIsClose);
 
 window.addEventListener('keydown', keyModalIsClose);
 
+window.addEventListener('keydown', keyPressNext);
 
+// window.addEventListener('keydown', keyPressPrev);
 
